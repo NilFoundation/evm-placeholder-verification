2261130a16e32d9ad78587caa3a9cebb363b8b21
Zerg1996 <alalmoskvin@gmail.com>
2022-04-25 21:01:16 +0200

    Add tests

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 cat "${1:-/dev/stdin}" > /.secret
 export LD_LIBRARY_PATH=/usr/local/lib/
+
 # shellcheck disable=SC2164
 cd /home/app/build/benchmark/
 ./run.sh /.secret
--- src/printTable.js
+++ src/printTable.js
@@ -2,7 +2,7 @@ asTable = require('as-table')
 const fs = require("fs");
 var file = 'time.log';
 
-names = ["Get Data", "Proof Generation Public", "Proof Generation Private", "Unified Addition"]
+names = ["Get Data", "Proof Generation Public", "Proof Generation Private", "Unified Addition", "Endo Scalar Mul", "Scalar Mul", "Unified Addition"]
 
 var text = fs.readFileSync(file).toString().trim();
 text = text.split("\n")
@@ -12,5 +12,8 @@ console.log(asTable.configure({delimiter: ' | '})([
     {Name: names[1], Time: text[1].split(" ")[1], Status: "Done", Verification_Gas: "-"},
     {Name: names[2], Time: text[2].split(" ")[1], Status: "Done", Verification_Gas: "-"},
     {Name: names[3], Time: text[3].split(" ")[1], Status: text[3].split(" ")[2], Verification_Gas: text[3].split(" ")[3]},
+    {Name: names[4], Time: text[4].split(" ")[1], Status: "Done", Verification_Gas: "-"},
+    {Name: names[5], Time: text[5].split(" ")[1], Status: "Done", Verification_Gas: "-"},
+    {Name: names[6], Time: text[6].split(" ")[1], Status: "Done", Verification_Gas: "-"},
     ])
 )
