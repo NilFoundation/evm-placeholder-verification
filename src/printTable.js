e16a85f2a8e27276210d54bf5a5846c56aed344d
Urickon <Rodoslavak@yandex.ru>
2022-05-03 19:13:59 +0500

    edit printTablejs

--- src/printTable.js
+++ src/printTable.js
@@ -1,19 +1,33 @@
-asTable = require('as-table')
+const asTable = require('as-table')
 const fs = require("fs");
-var file = 'time.log';
+const file = 'time.log';
 
-names = ["Get Data", "Proof Generation Public", "Proof Generation Private", "Unified Addition", "Endo Scalar Mul", "Scalar Mul", "Unified Addition"]
+const names = [
+    "Get Data",
+    "Proof Generation Public",
+    "Proof Generation Private",
+    "Unified Addition",
+    "Endo Scalar Mul",
+    "Scalar Mul",
+    "Unified Addition"
+]
 
 var text = fs.readFileSync(file).toString().trim();
 text = text.split("\n")
 
-console.log(asTable.configure({delimiter: ' | '})([
-    {Name: names[0], Time: text[0].split(" ")[1], Status: "Done", Verification_Gas: "-"},
-    {Name: names[1], Time: text[1].split(" ")[1], Status: "Done", Verification_Gas: "-"},
-    {Name: names[2], Time: text[2].split(" ")[1], Status: "Done", Verification_Gas: "-"},
-    {Name: names[3], Time: text[3].split(" ")[1], Status: text[3].split(" ")[2], Verification_Gas: text[3].split(" ")[3]},
-    {Name: names[4], Time: text[4].split(" ")[1], Status: "Done", Verification_Gas: "-"},
-    {Name: names[5], Time: text[5].split(" ")[1], Status: "Done", Verification_Gas: "-"},
-    {Name: names[6], Time: text[6].split(" ")[1], Status: "Done", Verification_Gas: "-"},
-    ])
-)
+function drawTable () {
+    return asTable.configure({delimiter: ' | '})(
+        names.forEach(drawTableRow)
+    )
+}
+
+function drawTableRow(name, index) {
+    return {
+        Name: name,
+        Time: text[index].split(" ")[1],
+        Status: index === 3 ? text[3].split(" ")[2] : 'Done',
+        Verification_Gas: index === 3 ? text[3].split(" ")[3] : '-'
+    }
+}
+
+console.log(drawTable())
