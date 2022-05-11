466aff96c32133814acec3c93096716c996109be
Ilias Khairullin <ilias@nil.foundation>
2022-05-11 14:35:14 +0300

    Fri tests updated.

--- contracts/commitments/test/public_api_fri_verification.sol
+++ contracts/commitments/test/public_api_fri_verification.sol
@@ -21,60 +21,88 @@ import "../../types.sol";
 import "../fri_verifier.sol";
 import "../batched_fri_verifier.sol";
 import "../../cryptography/transcript.sol";
+import "../../placeholder/proof_map_parser.sol";
 
 contract TestFriVerifier {
-    bool m_result;
-    uint256 m_proof_size;
-    types.fri_params_type m_params;
+    // bool m_result;
+    // uint256 m_proof_size;
+    // types.fri_params_type m_params;
 
-    function set_params(
-        uint256 modulus,
-        uint256 r,
-        uint256 max_degree
-    ) public {
-        m_params.modulus = modulus;
-        m_params.r = r;
-        m_params.max_degree = max_degree;
-    }
+    // function set_params(
+    //     uint256 modulus,
+    //     uint256 r,
+    //     uint256 max_degree
+    // ) public {
+    //     m_params.modulus = modulus;
+    //     m_params.r = r;
+    //     m_params.max_degree = max_degree;
+    // }
 
-    function set_q(uint256[] calldata q) public {
-        m_params.q = q;
-    }
+    // function set_q(uint256[] calldata q) public {
+    //     m_params.q = q;
+    // }
 
-    function set_D_omegas(uint256[] calldata D_omegas) public {
-        m_params.D_omegas = D_omegas;
-    }
+    // function set_D_omegas(uint256[] calldata D_omegas) public {
+    //     m_params.D_omegas = D_omegas;
+    // }
 
-    function set_U(uint256[] calldata U) public {
-        m_params.U = U;
-    }
+    // function set_U(uint256[] calldata U) public {
+    //     m_params.U = U;
+    // }
 
-    function set_V(uint256[] calldata V) public {
-        m_params.V = V;
-    }
+    // function set_V(uint256[] calldata V) public {
+    //     m_params.V = V;
+    // }
 
     // TODO: optimize - do not copy params from storage to memory
     function verify(
         bytes calldata raw_proof,
-        bytes calldata init_transcript_blob
+        bytes calldata init_transcript_blob,
+        // 0) modulus
+        // 1) r
+        // 2) max_degree
+        // 3) D_omegas_size
+        //  [..., D_omegas_i, ...]
+        // 4 + D_omegas_size) q_size
+        //  [..., q_i, ...]
+        uint256[] calldata init_params,
+        uint256[] calldata U,
+        uint256[] calldata V
     ) public {
         types.transcript_data memory tr_state;
         transcript.init_transcript(tr_state, init_transcript_blob);
-        m_result = fri_verifier.parse_verify_proof_be(
-            raw_proof,
-            0,
-            tr_state,
-            m_params
-        );
+        types.fri_params_type memory fri_params;
+        uint256 idx = 0;
+        fri_params.modulus = init_params[idx++];
+        fri_params.r = init_params[idx++];
+        fri_params.max_degree = init_params[idx++];
+        fri_params.D_omegas = new uint256[](init_params[idx++]);
+        for (uint256 i = 0; i < fri_params.D_omegas.length; i++) {
+            fri_params.D_omegas[i] = init_params[idx++];
+        }
+        fri_params.q = new uint256[](init_params[idx++]);
+        for (uint256 i = 0; i < fri_params.q.length; i++) {
+            fri_params.q[i] = init_params[idx++];
+        }
+        fri_params.U = U;
+        fri_params.V = V;
         require(
             raw_proof.length == fri_verifier.skip_proof_be(raw_proof, 0),
-            "FRI proof length is incorrect!"
+            "Fri proof length is not correct!"
         );
         require(
             raw_proof.length == fri_verifier.skip_proof_be_check(raw_proof, 0),
-            "FRI proof length is incorrect!"
+            "Fri proof length is not correct!"
         );
-        require(m_result, "FRI proof is not correct!");
+        require(
+            fri_verifier.parse_verify_proof_be(
+                raw_proof,
+                0,
+                tr_state,
+                fri_params
+            ),
+            "Fri proof verification failed!"
+        );
     }
 
     function batched_verify(
@@ -108,6 +136,7 @@ contract TestFriVerifier {
         for (uint256 i = 0; i < fri_params.q.length; i++) {
             fri_params.q[i] = init_params[idx++];
         }
+        placeholder_proof_map_parser.init(fri_params, fri_params.leaf_size);
         fri_params.batched_U = U;
         fri_params.batched_V = V;
         require(
--- contracts/logging.sol
+++ contracts/logging.sol
@@ -18,24 +18,49 @@
 pragma solidity >=0.8.4;
 
 library logging {
-    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+    function uint2decstr(uint256 _i)
+        internal
+        pure
+        returns (string memory _uintAsString)
+    {
         if (_i == 0) {
             return "0";
         }
-        uint j = _i;
-        uint len;
+        uint256 j = _i;
+        uint256 len;
         while (j != 0) {
             len++;
             j /= 10;
         }
         bytes memory bstr = new bytes(len);
-        uint k = len;
+        uint256 k = len;
         while (_i != 0) {
-            k = k-1;
-            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            k = k - 1;
+            uint8 temp = (48 + uint8(_i - (_i / 10) * 10));
             bytes1 b1 = bytes1(temp);
             bstr[k] = b1;
             _i /= 10;
+        }
+        return string(bstr);
+    }
+
+    function uint2hexstr(uint256 i) internal pure returns (string memory) {
+        if (i == 0) return "0";
+        uint256 j = i;
+        uint256 length;
+        while (j != 0) {
+            length++;
+            j = j >> 4;
+        }
+        uint256 mask = 15;
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        while (i != 0) {
+            uint256 curr = (i & mask);
+            bstr[--k] = curr > 9
+                ? bytes1(uint8(55 + curr))
+                : bytes1(uint8(48 + curr)); // 55 = 65 - 10
+            i = i >> 4;
         }
         return string(bstr);
     }
