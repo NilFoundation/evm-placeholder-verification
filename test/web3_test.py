4f7984b252b6605eccccd243d280797d5eea8eca
Ilias Khairullin <ilias@nil.foundation>
2022-05-13 13:36:46 +0300

    Tests updated.

---
+++ test/web3_test.py
@@ -0,0 +1,21 @@
+def find_compiled_contract(compiled, contract_name):
+    compiled_interface = False
+    for key, value in compiled.items():
+        if key.endswith(contract_name):
+            compiled_interface = value
+            break
+    else:
+        print(f'{contract_name} not found!')
+        exit(1)
+    return compiled_interface
+
+def write_tx_calldata(w3, tx_receipt, ofname = 'tx_calldata.txt'):
+    with open(ofname, 'w') as f:
+        f.write(w3.eth.get_transaction(tx_receipt.transactionHash).input)
+
+def print_tx_info(w3, tx_receipt, tx_name):
+    print(tx_name)
+    print(tx_receipt.transactionHash.hex())
+    print('gasUsed =', tx_receipt.gasUsed)
+    write_tx_calldata(w3, tx_receipt)
+
