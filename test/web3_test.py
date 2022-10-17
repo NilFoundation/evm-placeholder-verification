c5328d11fbf2b55db9e02f3441f61633163714f9
Ilias Khairullin <ilias@nil.foundation>
2022-10-17 18:29:11 +0300

    Test updated.

--- test/web3_test.py
+++ test/web3_test.py
@@ -83,3 +83,49 @@ def do_placeholder_verification_test_via_transact(test_contract_name, test_contr
     run_tx_receipt = w3.eth.wait_for_transaction_receipt(run_tx_hash)
     print_tx_info(w3, run_tx_receipt, params['_test_name'])
 
+
+def do_kimchi_verification_test_via_transact(test_contract_name, test_contract_path, linked_gates_entry_lib_name, linked_gates_libs_names, init_test_params_func):
+    w3 = init_connection()
+
+    print(f'{contracts_dir}/{test_contract_path}')
+    compiled = solcx.compile_files(
+        [f'{contracts_dir}/{test_contract_path}'],
+        output_values=['abi', 'bin'],
+        solc_version="0.8.12",
+        optimize=True,
+        optimize_runs=200)
+
+    compiled_gates_entry_lib_id, compiled_gates_entry_lib_interface = find_compiled_contract(
+        compiled, linked_gates_entry_lib_name)
+    gates_entry_lib_bytecode = compiled_gates_entry_lib_interface['bin']
+    gates_entry_lib_abi = compiled_gates_entry_lib_interface['abi']
+    gates_entry_lib_bytecode = deploy_link_libs(w3, compiled, gates_entry_lib_bytecode, linked_gates_libs_names)
+
+    gates_entry_contract_lib = w3.eth.contract(
+        abi=gates_entry_lib_abi, bytecode=gates_entry_lib_bytecode)
+    deploy_gates_entry_lib_tx_hash = gates_entry_contract_lib.constructor().transact()
+    deploy_gates_entry_lib_tx_receipt = w3.eth.wait_for_transaction_receipt(deploy_gates_entry_lib_tx_hash)
+
+    compiled_test_contract_id, compiled_test_contract_interface = find_compiled_contract(
+        compiled, test_contract_name)
+    bytecode = compiled_test_contract_interface['bin']
+    abi = compiled_test_contract_interface['abi']
+
+    bytecode = solcx.link_code(
+        bytecode,
+        {compiled_gates_entry_lib_id: deploy_gates_entry_lib_tx_receipt.contractAddress},
+        solc_version="0.8.12")
+
+    test_contract = w3.eth.contract(abi=abi, bytecode=bytecode)
+    deploy_tx_hash = test_contract.constructor().transact()
+    deploy_tx_receipt = w3.eth.wait_for_transaction_receipt(deploy_tx_hash)
+    print("Deployment:", deploy_tx_receipt.gasUsed)
+
+    test_contract_inst = w3.eth.contract(
+        address=deploy_tx_receipt.contractAddress, abi=abi)
+    params = init_test_params_func()
+    run_tx_hash = test_contract_inst.functions.verify(
+        params['proof'], params['init_params'], params['columns_rotations']).transact()
+    run_tx_receipt = w3.eth.wait_for_transaction_receipt(run_tx_hash)
+    print_tx_info(w3, run_tx_receipt, params['_test_name'])
+
