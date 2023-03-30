098fdc7fc5465cb8fe6782488e2ff6cd389ea638
e.tatuzova <e.tatuzova@nil.foundation>
2023-03-30 20:17:18 +0400

    Interface file for zkllvm examples compatibility temporary added #33

---
+++ contracts/gate_argument_interface.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.8.4;
+
+import "./types.sol";
+
+interface IGateArgument {
+    function evaluate_gates_be(
+        bytes calldata blob,
+        types.gate_argument_local_vars memory gate_params,
+        uint256 eval_proof_combined_value_offset,
+        types.arithmetization_params memory ar_params,
+        int256[][] calldata columns_rotations
+    ) external view returns (uint256 gates_evaluation);
+}
